595ee51d4ef1ee94ea220fae71fe4017c51eeda3
Kartike Gupta <2k23.cs2314010@gmail.com>
2026-06-17 06:48:43 +0000

    windowTheory

---
+++ framework.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+//framework for Sliding window pattern
+
+int main(){
+    //initialization
+    int low=0; //always
+    int high; // it can be 0 in case of variable window
+    //in case of variable window the pointer must be started from the start
+
+}
+
+
+
+
